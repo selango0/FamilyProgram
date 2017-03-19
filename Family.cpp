674fa2dae4994812e03a873d5a4ead8a75ce3683
selango2017 <shalmika.elango@gmail.com>
2017-03-19 16:19:42 -0400

    Update Family.cpp

--- Family.cpp
+++ Family.cpp
@@ -11,23 +11,24 @@ void Family::setName(char n[]) {
 char* Family::getName() {
 	return name;
 }
-vector<Person> Family::getParents() {
-	return Parent;
+Person Family::getParents()
+{
+	return Parents;
 }
-vector<Person> Family::getChildren() {
+Person Family::getChildren() {
 	return Children;
 }
-void Family::setParent(char FN[], char LN[], int a) {
+void Family::setParent(char FN[], char LN[], int a, int i) {
 	Parent[i].FN = FN;
 	Parent[i].LN = LN;
 	Parent[i].age = a;
 }
-void Family::setChildren(char FN[], char LN[], int a) {
+void Family::setChildren(char FN[], char LN[], int a, int i) {
 	Person child;
 	Children[i].FN = FN;
 	Children[i].LN = LN;
 	Children[i].age = a;
 
-	Children.push_back(child);
+	//Children.push_back(child);
 }
 
